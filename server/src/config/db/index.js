d6ad06cd73e36d697192aea564e7b3587173e451
vtp1201 <tiachopdo1201@gmail.com>
2022-04-05 16:22:26 +0700

    config db

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const dbHost = process.env.DB_HOST || 'localhost';
 const dbPort = process.env.DB_PORT || 27017;
 const dbName = process.env.DB_NAME || 'hrms';
-const uri = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+const uri = `mongodb://${dbHost}:${dbPort}/${dbName}` || process.env.MONGO_URI;
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://vtp1201:12012000@cluster0.akmfw.mongodb.net/hrms');
+        await mongoose.connect(uri);
         console.log('connect success');
     } catch (err) {
         console.log('Failed to connect to mongo on startup - retrying in 5 sec');
